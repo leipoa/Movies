e9b28c33b82bc7f348feadb4491a1eae2e48843c
Darya Vylomova <qwerty6108@gmail.com>
2023-06-21 22:51:56 +0300

    8 commit

--- src/test/java/MoviesManagerTest.java
+++ src/test/java/MoviesManagerTest.java
@@ -11,7 +11,7 @@ public class MoviesManagerTest {
 
         manager.add("1");
 
-        String[] expected = { "1" };
+        String[] expected = {"1"};
         String[] actual = manager.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
@@ -20,7 +20,7 @@ public class MoviesManagerTest {
 
     @Test
     public void testFindLast() {
-        MoviesManager manager = new MoviesManager(10);
+        MoviesManager manager = new MoviesManager();
 
         manager.add("1");
         manager.add("2");
@@ -33,10 +33,55 @@ public class MoviesManagerTest {
         manager.add("9");
         manager.add("10");
 
-        String[] expected = { "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
+        String[] expected = {"10", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void testFindLastBelowLimit() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.add("1");
+        manager.add("2");
+        manager.add("3");
+        manager.add("4");
+        manager.add("5");
+        manager.add("6");
+        manager.add("7");
+        manager.add("8");
+        manager.add("9");
+
+
+        String[] expected = {"9", "8", "7", "6", "5", "4", "3", "2", "1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testFindLastAboveLimit() {
+        MoviesManager manager = new MoviesManager(11);
+
+        manager.add("1");
+        manager.add("2");
+        manager.add("3");
+        manager.add("4");
+        manager.add("5");
+        manager.add("6");
+        manager.add("7");
+        manager.add("8");
+        manager.add("9");
+        manager.add("10");
+        manager.add("11");
+
+        String[] expected = {"11", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
 }
